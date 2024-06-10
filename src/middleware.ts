756b6128a3127b2b42ef8492cfc90230c3d3b644
Orchemi <tmdgns1126@naver.com>
2024-06-11 00:02:06 +0900

    Fix: middleware protectedPage 구현

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 /**
  * 지금은 로그인 상태일 때 로그인, 회원가입 페이지 접근만 리다이렉션 처리하고 나머지는 주석 처리함
@@ -9,22 +9,22 @@ import type { NextRequest } from 'next/server';
 
 // 1. auth page 와 로그인 이후 접근 가능한 protected page 분류
 const AUTH_PAGES = ['/login', '/register'];
-// const PROTECTED_PAGES = [];
+const PROTECTED_PAGES = ['/profile', '/profile/*'];
 
 export function middleware(request: NextRequest) {
   // 2. 경로 확인해서 auth page와 protected page 구분
   const { nextUrl, cookies } = request;
   const path = nextUrl.pathname;
-  // const isProtectedPage = PROTECTED_PAGES.includes(path);
+  const isProtectedPage = PROTECTED_PAGES.some((page) => path.startsWith(page));
   const isAuthPage = AUTH_PAGES.includes(path);
 
   // 3. 쿠키로 로그인 유무 확인
   const accessToken = cookies.get('accessToken');
 
   // 4. 로그인 안된 상태로 protected page 접근 시 리다이렉션
-  // if (isProtectedPage && !accessToken) {
-  //   return NextResponse.redirect(new URL('/login', request.nextUrl));
-  // }
+  if (isProtectedPage && !accessToken) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
+  }
 
   // 5. 로그인 된 상태로 auth page 접근 시 메인 페이지로 리다이렉션
   if (isAuthPage && accessToken) {
